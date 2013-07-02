7411dad8d0b7a65d9b68abf4914c6d0911fb830e
Gareth Senior <gareth53@gmail.com>
2013-07-02 21:29:56 +0100

    more tests

--- jasmine-tests/spec/PuzSpec.js
+++ jasmine-tests/spec/PuzSpec.js
@@ -8,6 +8,15 @@ describe("Puzzle", function() {
         body.appendChild(test_wrapper);
   });
 
+  afterEach(function() {
+        var body = document.getElementsByTagName('body')[0];
+        var fixture = document.getElementById('test_wrapper');
+        body.removeChild(fixture);
+        var fixture2 = document.getElementById('test_wrapper2');
+        if (fixture2) body.removeChild(fixture2);
+  });
+  
+
   it("should build a puzzle board in a DOM element specified by ID", function() {
     // first witha specified ID that returns a DOM element
     puzzle = new Puz('test_wrapper');
@@ -19,18 +28,103 @@ describe("Puzzle", function() {
     var wrap = puzzle.get_board_wrapper();
     expect(wrap.id).toBe('test_wrapper2');
     expect(wrap.tagName.toLowerCase()).toBe('div');
-
-
   });
-
-
     
   it("should build a puzzle board", function() {
-    puzzle = new Puz();
+    puzzle = new Puz('test_wrapper');
     puzzle.init();
     expect(document.getElementById(puzzle.board_wrapper_id)).toBeTruthy();
   });
   
+  
+  it("should build a board map", function() {
+        var settings = [
+        // w, h
+        [4, 4],
+        [5, 4],
+        [6, 4],
+        [7, 4],
+        [4, 5],
+        [4, 6],
+        [4, 7],
+        [4, 8],
+        [57, 102]
+        ];
+        for (var i=0, lim=settings.length; i<lim ; i++) {
+          var w = settings[i][0],
+              h = settings[i][1];
+          puzzle = new Puz('test', {
+              'board_w': w,
+              'board_h': h
+          });
+          puzzle.build_board_map()
+          expect(puzzle.board.length).toBe(h);
+          expect(puzzle.board[0].length).toBe(w);
+        }
+  });
+  it("should understand which tiles are movable", function() {
+      var tests = [
+        {   'board' : [
+                [ 1, 2, 3, 4],
+                [ 5, 6, 7, 8],
+                [ 9,10,11,12],
+                [13,14,15, 0],
+            ],
+            'space': [3, 3],
+            'movables': [12, 15]
+        },
+        {   'board' : [
+                [ 1, 2, 3, 0],
+                [ 5, 6, 7, 8],
+                [ 9,10,11,12],
+                [13,14,15, 4],
+            ],
+            'space': [3, 0],
+            'movables': [3, 8]
+        },
+        {   'board' : [
+                [ 0, 2, 3, 4],
+                [ 5, 6, 7, 8],
+                [ 9,10,11,12],
+                [13,14,15, 1],
+            ],
+            'space' : [0, 0],
+            'movables': [2, 5]
+        },
+        {   'board' : [
+                [ 1, 2, 3, 4],
+                [ 5, 6, 7, 8],
+                [ 9, 0,11,12],
+                [13,14,15,10],
+            ],
+            'space': [1, 2],
+            'movables': [6, 9, 11, 14]
+        },
+        {   'board' : [
+                [ 1, 2, 3, 4],
+                [ 5, 6, 0, 8],
+                [ 9, 7,11,12],
+                [13,14,15,10],
+            ],
+            'space': [2, 1],
+            'movables': [3, 6, 8, 11]
+        }
+        ];
+
+        for (var i=0, lim=tests.length; i<lim ; i++) {
+          puzzle = new Puz('test', {
+              'board_w': 4,
+              'board_h': 4
+          });
+          puzzle.space = tests[i]['space'];
+          puzzle.board = tests[i]['board'];
+
+          var output = puzzle.movable_tiles();
+          output.sort(function(a, b) { return (a > b) });
+          expect(output).toEqual(tests[i]['movables']);
+        }
+  });
+
   it("should be able to format times in a human-friendly way", function() {
       puzzle = new Puz();
       times = [
@@ -51,7 +145,7 @@ describe("Puzzle", function() {
   });
 
   it("should be create a board map", function() {
-      var puzzle = new Puz();
+      puzzle = new Puz();
       // default is 4x4
       puzzle.build_board_map();
       expect(puzzle.board.length).toBe(4);
